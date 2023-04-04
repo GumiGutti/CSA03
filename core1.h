d10f90b8ac3b4ecba20d7246fa51dc1b14d0f6d9
Gusztav, Holler <121695723+GumiGutti@users.noreply.github.com>
2023-04-04 23:35:50 +0200

    Update core1.h

--- core1.h
+++ core1.h
@@ -27,4 +27,56 @@
 
 // #include <OneWire.h>
 // #include <DallasTemperature.h>
-#include <EEPROM.h>
+// #include <EEPROM.h>
+
+
+TaskHandle_t hCore1task;
+
+uint32_t tLoraTrigger = 0;
+uint32_t tLoraDelay = 500;
+
+taskState sLora = sStart;
+bool firstRunCore1 = true;
+
+void core1task(void* parameter);
+
+void core1setup(void) {  // a.k.a. setup
+  xTaskCreatePinnedToCore(
+    core1task,
+    "core1task",
+    10000,
+    NULL,
+    0,
+    &hCore1task,
+    1);
+}
+
+
+void core1task(void* parameter) {  // a.k.a. loop
+  for (;;) {
+    if (firstRunCore1) {
+      firstRunCore1 = false;
+    }
+    // lora ....
+    if (millis() - tLoraTrigger > tLoraDelay) {
+      tLoraTrigger = millis();
+      switch (sLora) {
+        case sRun:
+          {
+            break;
+          }
+        case sError:
+          {
+            break;
+          }
+        case sStart:
+          {
+            break;
+          }
+        default:
+          {  //itt baj van....}
+          }
+      }
+    }
+  }
+}
