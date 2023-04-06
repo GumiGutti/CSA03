3d060503bfee7ff86f00ab19b47f846ffb97cba9
Gusztav, Holler <121695723+GumiGutti@users.noreply.github.com>
2023-04-06 13:27:16 +0200

    mutex kezdemény
    még nem fordult

--- cansat03.h
+++ cansat03.h
@@ -4,7 +4,31 @@
 #include "freertos/task.h"
 #include "freertos/semphr.h"
 
-SemaphoreHandle_t xMutex;
+SemaphoreHandle_t i2cSemaphore;
+void createSemaphore(SemaphoreHandle_t &sem){
+    &sem = xSemaphoreCreateMutex();
+    xSemaphoreGive( ( sem) );
+}
+
+// Lock the variable indefinietly. ( wait for it to be accessible )
+void lock(SemaphoreHandle_t &sem){
+    xSemaphoreTake(sem, portMAX_DELAY);
+}
+
+// give back the semaphore.
+void unlock(SemaphoreHandle_t &sem){
+    xSemaphoreGive(sem);
+}
+
+SemaphoreHandle_t xTemp; // temperature, calculated, fusion
+SemaphoreHandle_t xPressure; // calculated, fusion
+// SemaphoreHandle_t ; //
+// SemaphoreHandle_t ; //
+// SemaphoreHandle_t ; //
+// SemaphoreHandle_t ; //
+// SemaphoreHandle_t ; //
+// SemaphoreHandle_t ; //
+// SemaphoreHandle_t ; //
 
 
 // TO általában: TimeOut
@@ -28,12 +52,17 @@ typedef struct statusfield_t {
 
 statusfield_t status;
 
+union uStatusUnion {
+  statusfield_t statusBits;
+  char statusChars[2];
+} uStatus;
+
 #define loraRst 0    // OUTPUT ONLY!
 #define GPIO01 1     // N/A
 #define loraTx 2     // generic
 #define GPIO03 3     // N/A
 #define loraRx 4     // generic
-#define GPIO5 5      // generic
+#define pinPin 5      // generic
 #define GPIO0 6      // N/A
 #define GPIO0 7      // N/A
 #define GPIO0 8      // N/A
@@ -46,7 +75,7 @@ statusfield_t status;
 #define GPIO15 15    // generic
 #define gpsRx 16     // generic
 #define gpsTx 17     // generic
-#define GPIO18 18    // generic
+#define pinPon 18    // generic
 #define GPIO19 19    // generic
 #define GPIO20 20    // N/A
 #define i2cSDA 21    // generic
