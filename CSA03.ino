5f99d6a4a45857c139b59c7d9f34fa8a3112b123
Gusztav, Holler <121695723+GumiGutti@users.noreply.github.com>
2023-04-06 19:37:47 +0200

    vtaskdelay + while

--- CSA03.ino
+++ CSA03.ino
@@ -1,14 +1,17 @@
 #include "Arduino.h"
 #include <HardwareSerial.h>
 #include "cansat03.h"
-HardwareSerial s(0);     // use UART0 serial debug or unit A-B intercom
-HardwareSerial lora(1);  // use UART1 
-HardwareSerial gps(2);   // use UART2 
-#include "core0.h"
-#include "core1.h"
 
 #define CORE0TASKPRIO 3
 #define CORE1TASKPRIO 3
+
+HardwareSerial s(0);     // use UART0 serial debug or unit A-B intercom
+HardwareSerial lora(1);  // use UART1
+HardwareSerial gps(2);   // use UART2
+
+#include "core0.h"
+#include "core1.h"
+
 
 void setup() {
 
@@ -16,11 +19,36 @@ void setup() {
   s.println();
   s.println("CANSAT boot....");
 
+  xTemp = xSemaphoreCreateMutex();
+  xSemaphoreGive((xTemp));
+
+
+  SemaphoreHandle_t xTemp;      // temperature, calculated, fusion: DS, BME, BMP, 6500
+SemaphoreHandle_t xPressure;  // calculated, fusion: BME, BMP
+SemaphoreHandle_t xHumidity;  // measured (BME)
+SemaphoreHandle_t xImuok; //
+SemaphoreHandle_t xBmpok; //
+SemaphoreHandle_t xBmeok; //
+SemaphoreHandle_t xAdxlok; //
+SemaphoreHandle_t xInaok; //
+SemaphoreHandle_t xDsok; //
+SemaphoreHandle_t xPosx; //
+SemaphoreHandle_t xPosy; //
+SemaphoreHandle_t xPosz; //
+SemaphoreHandle_t xQa; //
+SemaphoreHandle_t xQi; //
+SemaphoreHandle_t xQj; //
+SemaphoreHandle_t xQk; //
+SemaphoreHandle_t xLat; //
+SemaphoreHandle_t xLon; //
+SemaphoreHandle_t xAlt; //
+
   core0setup();
   core1setup();
 
+  while (1) vTaskDelay(1);
 }
 
-void loop(){
+void loop() {
   // empty on purpose
 }
