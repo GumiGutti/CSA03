1c44c9bc78966b309bc704a1a266a764b78890a4
Gusztav, Holler <121695723+GumiGutti@users.noreply.github.com>
2023-04-10 02:02:34 +0200

    ringbuffer cuccok

---
+++ uart.h
@@ -0,0 +1,42 @@
+const uint32_t ringSize = 4096;
+byte sdring[ringSize];
+uint32_t sdhead = 0;
+uint32_t sdtail = 0;
+
+
+uint32_t ringBytes(void) {
+  return sdhead > sdtail ? sdhead - sdtail : sdhead + ringSize - sdtail;
+}
+uint32_t ringFree(void) {
+  return ringSize - 1 - ringBytes();
+}
+
+void headPP() {
+  sdhead++;
+  if (sdhead == ringSize) sdhead = 0;
+}
+
+void tailPP() {
+  sdtail++;
+  if (sdtail == ringSize) sdtail = 0;
+}
+
+void ringSend(uint32_t chunkSize) {
+  if(ringBytes() >= chunkSize){
+    while(chunkSize--){
+      s.write(sdring[sdtail]);
+      tailPP();
+    }
+  }
+}
+
+void addSD(char* karcsi) {
+  if (ringFree() > strlen(karcsi)) {
+    while (*karcsi) {
+      sdring[sdhead] = *karcsi;
+      karcsi++;
+      headPP();
+    }
+  } else
+    bufferOK = 0;
+}
